8f65be59f6cd32827c2f64c0ff632cebdc1cd71b
Wesxdz <sirwesleybarlow@gmail.com>
2022-02-05 08:06:04 -0600

    Update files :)

--- src/build.sh
+++ src/build.sh
@@ -12,10 +12,10 @@ rm -rf ../site
 mkdir ../site
 
 # Linux(debug)
-cc -std=c99 -DDEBUG -Wall -Wno-unknown-pragmas -Wpedantic -Wshadow -Wuninitialized -Wextra -Werror=implicit-int -Werror=incompatible-pointer-types -Werror=int-conversion -Wvla -g -Og -fsanitize=address -fsanitize=undefined main.c -o main
+# cc -std=c99 -DDEBUG -Wall -Wno-unknown-pragmas -Wpedantic -Wshadow -Wuninitialized -Wextra -Werror=implicit-int -Werror=incompatible-pointer-types -Werror=int-conversion -Wvla -g -Og -fsanitize=address -fsanitize=undefined main.c -o main
 
 # Linux(fast)
-# cc main.c -std=c89 -Os -DNDEBUG -g0 -s -Wall -Wno-unknown-pragmas -o main
+cc main.c -std=c89 -Os -DNDEBUG -g0 -s -Wall -Wno-unknown-pragmas -o main
 
 # RPi
 # tcc -Wall main.c -o main
